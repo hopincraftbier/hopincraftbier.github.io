056df9977a8e70aa76665751eddc4e7ce2794de4
Jochen <jowyl@jowyl.com>
2025-06-18 11:26:46 +0200

    added hiding of header

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,1 +1,27 @@
 console.log("HopInCraftbier custom js");
+/* Get the header element and it's position */
+var headerDiv = document.querySelector("#tile-header-fcHJMd");
+
+if (headerDiv) {
+  var prevScrollpos = window.pageYOffset;
+  var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+  var pos = "-" + document.querySelector(".ins-tile--header.ins-tile--left-logo-detailed .ins-header__row:nth-child(1)").offsetHeight + "px";
+
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if(Math.abs(prevScrollpos - currentScrollPos) <= 5)
+      return;
+
+    /* if we're scrolling up, or we haven't passed the header,
+       show the header at the top */
+    if (prevScrollpos > currentScrollPos  || currentScrollPos < headerBottom){
+      headerDiv.style.top = "0";
+    }
+    else{
+      /* otherwise we're scrolling down & have passed the header so hide it */
+      headerDiv.style.top = pos;
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+}
