f2608ecf9c7261e091c6c323d731e0886ba6b41d
Jochen <jowyl@jowyl.com>
2025-06-18 11:32:10 +0200

    added hiding of header

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,27 +1,25 @@
 console.log("HopInCraftbier custom js");
 /* Get the header element and it's position */
-var headerDiv = document.querySelector("#tile-header-fcHJMd");
+const headerDiv = document.querySelector("#tile-header-fcHJMd");
 
 if (headerDiv) {
-  var prevScrollpos = window.pageYOffset;
-  var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
-  var pos = "-" + document.querySelector(".ins-tile--header.ins-tile--left-logo-detailed .ins-header__row:nth-child(1)").offsetHeight + "px";
+  const pos = "-" + document.querySelector(".ins-tile--header.ins-tile--left-logo-detailed .ins-header__row:nth-child(1)").offsetHeight + "px";
+  let prevScrollPos = window.scrollY;
+  let headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
 
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if(Math.abs(prevScrollpos - currentScrollPos) <= 5)
-      return;
+  window.onscroll = function () {
+    let currentScrollPos = window.scrollY;
+    if (Math.abs(prevScrollPos - currentScrollPos) <= 5) return;
 
     /* if we're scrolling up, or we haven't passed the header,
        show the header at the top */
-    if (prevScrollpos > currentScrollPos  || currentScrollPos < headerBottom){
+    if (prevScrollPos > currentScrollPos || currentScrollPos < headerBottom) {
       headerDiv.style.top = "0";
-    }
-    else{
+    } else {
       /* otherwise we're scrolling down & have passed the header so hide it */
       headerDiv.style.top = pos;
     }
 
-    prevScrollpos = currentScrollPos;
+    prevScrollPos = currentScrollPos;
   }
 }
