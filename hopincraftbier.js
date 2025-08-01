fc43ba9a08e7b4ff0c81228803bdfea95093b1a9
Jochen <jowyl@jowyl.com>
2025-08-01 10:26:15 +0200

    cart info msg

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-console.log("HopInCraftbier custom js v3");
+console.log("HopInCraftbier custom js v4");
 /* Get the header element and it's position */
 document.txtNl1 = '<div id="discountContainer"><div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
 document.txtNl2 = '</td><td> - </td></tr><tr><td class="header">Overschrijving</td><td>€ ';
@@ -37,7 +37,14 @@ if (headerDiv) {
     prevScrollPos = currentScrollPos;
   }
 }
-var cartTotalMo = new MutationObserver(function(ms) {
+  var priceO = new MutationObserver(function(ms) {
+    ms.forEach(function (m) {
+      addCouponInfo(false);
+      soonLabel();
+      processAttributes();
+    })
+  });
+  var cartTotalMo = new MutationObserver(function(ms) {
   ms.forEach(function (m) {
     for (var i = 0; i < m.addedNodes.length; i++) {
       if (m.addedNodes[i].nodeType == Node.ELEMENT_NODE) {
