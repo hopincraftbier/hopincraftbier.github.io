7d1a6bb4211a94e68845552cf6d70d5f51d938fc
Jochen <jowyl@jowyl.com>
2025-09-27 13:10:39 +0200

    move subtitle

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-console.log("HopInCraftbier custom js v4.40");
+console.log("HopInCraftbier custom js v4.41");
 /* Get the header element and it's position */
 document.txtNl1 = '<div id="discountContainer"><div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
 document.txtNl2 = '</td><td> - </td></tr><tr><td class="header">Overschrijving</td><td>€ ';
@@ -51,7 +51,7 @@ if (headerDiv) {
       if (m.addedNodes[i].nodeType === Node.ELEMENT_NODE) {
         if (typeof m.addedNodes[i].className == "string") {
           const className = m.addedNodes[i].className;
-          // console.log('-> ' + className);
+          console.log('-> ' + className);
           if (className.indexOf('ec-store ec-store__product-page') >= 0) {
             addCouponInfo(true);
             soonLabel();
@@ -65,7 +65,8 @@ if (headerDiv) {
           } else if (className.indexOf('grid__wrap-inner') >= 0) {
             moveSubtitle();
           } else if (className.indexOf('ec-store ec-store__category-page') >= 0 ||
-            className.indexOf('ec-store ec-store__search-page') >= 0) {
+            className.indexOf('ec-store ec-store__search-page') >= 0 ||
+            className.indexOf('ec-related-products')) {
             processExpectedLabels();
             processAttributes();
             processStock();
@@ -232,6 +233,7 @@ function addCouponInfo(initial) {
 }
 
 function moveSubtitle() {
+  console.log('moveSubtitle');
   document.querySelectorAll('div.grid-product__wrap-inner div.grid-product__subtitle').forEach(function (p) {
     var imgWrapElement = p.parentElement.querySelector('div.grid-product__image-wrap');
     if (imgWrapElement) {
