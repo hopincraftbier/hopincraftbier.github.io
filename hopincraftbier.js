cf52b8d695baf1e2b164b0f081ebd9b941d36ff5
Jochen <jowyl@jowyl.com>
2025-09-28 23:43:37 +0200

    redirect

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-console.log("HopInCraftbier custom js v4.56");
+console.log("HopInCraftbier custom js v4.57");
 /* Get the header element and it's position */
 document.txtNl1 = '<div id="discountContainer"><div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
 document.txtNl2 = '</td><td> - </td></tr><tr><td class="header">Overschrijving</td><td>€ ';
@@ -240,6 +240,7 @@ function addCouponInfo(initial) {
 }
 
 function moveSubtitle() {
+  redirectWhenNeeded();
   console.log('moveSubtitle');
   document.querySelectorAll('div.grid-product__wrap-inner div.grid-product__subtitle').forEach(function (p) {
     var imgWrapElement = p.parentElement.querySelector('div.grid-product__image-wrap');
