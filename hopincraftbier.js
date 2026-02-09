655169c2397cee7d8c404b3529a88d540ee41416
hopincraftbier <info@hopincraftbier.be>
2026-02-09 11:36:54 +0100

    cart styling

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-const version = 'v6.34';
+const version = 'v6.35';
 const txtNl1 = '<div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
 const txtNl2 = '</td><td> - </td></tr><tr><td class="header">Overschrijving</td><td>€ ';
 const txtNl3 = '</td><td>€ ';
@@ -571,9 +571,9 @@ function translateCheckoutNotice() {
     log('translateCheckoutNotice');
     let element;
     if ('EN' === getCustomerLng()) {
-        element = document.querySelector('span.adb_nl');
+        element = document.querySelector('.adb_nl');
     } else {
-        element = document.querySelector('span.adb_en');
+        element = document.querySelector('.adb_en');
     }
     if (element) {
         element.style.display = 'none';
