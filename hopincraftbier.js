bfeb5e6e7f3261082ef8b1caa61ef1f2c082cb12
hopincraftbier <info@hopincraftbier.be>
2026-02-16 14:05:11 +0100

    test shipping info

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-const version = 'v6.44';
+const version = 'v6.45';
 const txtNl1 = '<div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
 const txtNl2 = '</td><td> - </td></tr><tr><td class="header">Overschrijving</td><td>€ ';
 const txtNl3 = '</td><td>€ ';
@@ -705,7 +705,7 @@ function removeCountries() {
             if (!optionElement.hasAttribute('disabled') &&
                 optionElement.value &&
                 optionElement.value !== '' &&
-                optionElement.value.length > 3 &&
+                optionElement.value.length <= 3 &&
                 countries.indexOf(optionElement.value) < 0) {
                 optionElement.setAttribute('disabled', true);
                 optionElement.style.display = 'none';
