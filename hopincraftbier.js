297207a696c14b5a2aeff998e08c50171726c33d
hopincraftbier <info@hopincraftbier.be>
2026-06-10 19:50:06 +0200

    try expected as btn

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-const version = 'v7.13';
+const version = 'v7.15';
 let currentLanguage;
 
 const txtNl1 = '<div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
@@ -443,8 +443,10 @@ function processExpectedLabels() {
                     || buyNowEl?.textContent === 'Uitverkocht'
                     || buyNowEl?.textContent === 'Reserveer'
                     || buyNowEl?.textContent === 'Reserver') {
-                if (buyNowEl.style.display !== 'none') {
-                    buyNowEl.style.display = 'none';
+                if (buyNowEl?.textContent !== 'Reserveer' && buyNowEl?.textContent !== 'Reserver') {
+                    if (buyNowEl.style.display !== 'none') {
+                        buyNowEl.style.display = 'none';
+                    }
                 }
                 if (document.querySelector('h1.page-title__name.ec-header-h1')?.textContent?.trim() !== 'Pre-order') {
                     let priceEl = p.querySelector('div.grid-product__price');
