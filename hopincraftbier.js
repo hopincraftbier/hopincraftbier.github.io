31d2de2946c21628d81e31d8fb57a39cd02c51ee
hopincraftbier <info@hopincraftbier.be>
2026-08-15 20:05:44 +0200

    reservations

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-const version = 'v7.43';
+const version = 'v7.44';
 let currentLanguage;
 
 const txtNl1 = '<div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
@@ -316,9 +316,9 @@ function processAttributes(status) {
                 if (buttonEl) {
                     buttonEl.classList.add('dtooltip');
                     if ('EN' === lng) {
-                        buttonEl.insertAdjacentHTML('beforeend', '<p class="dtooltiptext">5% korting op reservaties.</p>');
-                    } else {
                         buttonEl.insertAdjacentHTML('beforeend', '<p class="dtooltiptext">5% discount on reservations.</p>');
+                    } else {
+                        buttonEl.insertAdjacentHTML('beforeend', '<p class="dtooltiptext">5% korting op reservaties.</p>');
                     }
                 }
             }
