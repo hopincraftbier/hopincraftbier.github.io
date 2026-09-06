03a518253baec048c404540d41a10a24f5c0df22
hopincraftbier <info@hopincraftbier.be>
2026-09-06 09:36:29 +0200

    beer lovers day

--- hopincraftbier.js
+++ hopincraftbier.js
@@ -1,4 +1,4 @@
-const version = 'v7.56';
+const version = 'v7.57';
 let currentLanguage;
 
 const txtNl1 = '<div class="dtooltip"><p class="hover question">Kortingscoupon</p><p class="dtooltiptext">Afhankelijk van de gekozen betaling en levering, kunt u een kortingscoupon krijgen die te gebruiken is bij een volgende bestelling. Voor dit bier ziet u de bedragen in deze tabel</p></div><table class="discount-table"><thead><tr class="first_header"><th></th><th colspan="2">Manier van levering</th></tr><tr><th>Manier van betaling</th><th>Afhaling</th><th>Levering</th></tr></thead><tbody><tr><td class="header">Betalen bij afhaling</td><td>€ ';
@@ -713,6 +713,7 @@ function renameBuyButtonToPreorder() {
                     labelEl.className.indexOf('grid-product__label--Nieuw') < 0 &&
                     labelEl.className.indexOf('grid-product__label--New') < 0 &&
                     labelEl.className.indexOf('grid-product__label--Last-one') < 0 &&
+                    labelEl.className.indexOf('grid-product__label--Beer-Lover0027s-Day') < 0 &&
                     labelEl.className.indexOf('grid-product__label--Laatste') < 0) {
                     buttonTextEl.innerHTML = btnTxt;
                 }
